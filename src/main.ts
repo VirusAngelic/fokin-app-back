c6370443909a1779309ca351b38ca3871f472e6d
Moises Diaz <daxter4689@gmail.com>
2024-07-16 20:38:25 -0600

    feat:DEV:Agregada configuracion de CORS para desarrollo

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,21 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 
+const corsConfig = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
     { logger: ['error', 'warn', 'log'] },
   );
+  app.enableCors(corsConfig);
   await app.listen(3000, '192.168.3.5');
 }
 bootstrap();
